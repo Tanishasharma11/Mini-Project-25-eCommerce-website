82d41645235b8fcabea4950275d543593a1b79dc
Tanisha Sharma <92216346+Tanishasharma11@users.noreply.github.com>
2023-06-27 21:15:59 +0530

    Login.jsx

--- tss-react-ecommerce-main/src/pages/Login.jsx
+++ tss-react-ecommerce-main/src/pages/Login.jsx
@@ -1,36 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../components";
-import axios from "axios";
 
 const Login = () => {
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
-  const onChangeEmail=(e)=>{
-    console.log(e.target.value)
-    setEmail(e.target.value);
-  }
-  const onChangePasword=(e)=>{
-    console.log(e.target.value)
-    setPassword(e.target.value);
-  }
-
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    console.log("handle Submit Called")
-    console.log("Email: ",email)
-    console.log("Password: ",password)
-
-    axios.post('http://localhost:5000/api/login',{
-      email:email,
-      password:password
-    }).then(res=>{
-      console.log(res.data)
-    }).catch(err=>{
-      console.log(err)
-    })
-  }
-
   return (
     <>
       <Navbar />
@@ -39,7 +11,7 @@ const Login = () => {
         <hr />
         <div className="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-            <form onSubmit={handleSubmit} >
+            <form>
               <div className="my-3">
                 <label for="display-4">Email address</label>
                 <input
@@ -47,10 +19,6 @@ const Login = () => {
                   className="form-control"
                   id="floatingInput"
                   placeholder="name@example.com"
-                  onChange={onChangeEmail}
-                  name="email"
-                  value={email}
-
                 />
               </div>
               <div className="my-3">
@@ -60,16 +28,13 @@ const Login = () => {
                   className="form-control"
                   id="floatingPassword"
                   placeholder="Password"
-                  onChange={onChangePasword}
-                  name="password"
-                  value={password}
                 />
               </div>
               <div className="my-3">
                 <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
               </div>
               <div className="text-center">
-                <button className="my-2 mx-auto btn btn-dark" type="submit" >
+                <button className="my-2 mx-auto btn btn-dark" type="submit" disabled>
                   Login
                 </button>
               </div>
